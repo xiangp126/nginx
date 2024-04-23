50b2a9e950fdedcb5b1501a1bc916e8a179860f4
Peng Xiang <xiangp@fortinet.com>
2024-04-23 00:00:16 -0700

    tls1.3 http2 and new index.html

--- onekey.sh
+++ onekey.sh
@@ -1,47 +1,24 @@
 #!/bin/bash
 
-CFLAGS="-pipe -W -Wall -Wpointer-arith -Wno-unused -g3"
 prefix="/opt/nginx"
-patch_file="patch/nginx-1.9.2.patch"
-# checkout_tag="release-1.9.2"
-checkout_tag="learn-1.9.2"
+openssl_path="/data/openssl"
 user_notation="@@@@"
 
-git branch | grep "$checkout_tag" 2>&1 > /dev/null
-if [ $? -ne 0 ]; then
-    echo "$user_notation Checking out to tag $checkout_tag"
-    git checkout $checkout_tag
-    if [ $? -ne 0 ]; then
-        echo "$user_notation git checkout failed"
-        exit 1
-    fi
-else
-    echo "$user_notation Already in branch $checkout_tag"
-
-fi
-
+CFLAGS="-pipe -W -Wall -Wpointer-arith -Wno-unused -g3"
 export CFLAGS
 # ./auto/configure --prefix=/opt/nginx --with-threads --with-http_ssl_module
-./auto/configure --prefix=$prefix --with-threads
+    # --with-openssl=$openssl_path \
+./auto/configure \
+    --prefix=$prefix \
+    --with-threads \
+    --with-http_ssl_module \
+    --with-http_stub_status_module \
+    --with-http_gzip_static_module \
+    --with-http_v2_module
+
 if [ $? -ne 0 ]; then
     echo "$user_notation configure failed"
     exit 1
-fi
-
-# Check if need to apply patch
-grep "CRYPT_DATA_INTERNAL_SIZE" src/os/unix/ngx_user.c
-if [ $? -ne 0 ]; then
-    if [ ! -f $patch_file ]; then
-        echo "$user_notation Patch file $patch_file does not exist"
-        exit 1
-    fi
-    git apply $patch_file
-    if [ $? -ne 0 ]; then
-        echo "$user_notation Apply patch failed"
-    	exit 1
-    fi
-else
-    echo "$user_notation Patch already applied"
 fi
 
 make -j12
@@ -55,8 +32,8 @@ exit 0
 
 if [ -d $prefix ]; then
     if [ "$(ls -A $prefix)" ]; then
-	echo "$user_notation Destination directory $prefix exists and not empty, delete it first"
-	sudo rm -rf $prefix
+        echo "$user_notation Destination directory $prefix exists and not empty, delete it first"
+        sudo rm -rf $prefix
     fi
 fi
 
